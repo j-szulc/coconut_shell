2b5a27de460f5fbde4eb6f462f19acd775ea636a
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2023-09-27 01:25:32 +0200

    Remove leftover scratch files

--- test3.sh
+++
@@ -1,5 +0,0 @@
-rm -f /tmp/dupa || true
-mkfifo /tmp/dupa
-
-seq 10000000 > /tmp/dupa &
-cat /tmp/dupa | wc -l
--- test4.sh
+++
@@ -1,4 +0,0 @@
-rm /tmp/test.sock || true
-nc -lU  /tmp/test.sock | wc -l &
-seq 10000000 | nc -U /tmp/test.sock
-wait
